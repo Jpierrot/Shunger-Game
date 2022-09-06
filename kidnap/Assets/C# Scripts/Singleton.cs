2139a18e9b34774e7600e38a846c77787cebff34
Jpie <s20015@gsm.hs.kr>
2022-09-07 02:06:42 +0900

    싱글톤 구현

---
+++ kidnap/Assets/C# Scripts/Singleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kidnap
+{
+    class Singleton
+    {
+    }
+}
