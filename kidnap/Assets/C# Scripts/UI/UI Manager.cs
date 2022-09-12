ce86d4fbf93dd1f340850df30b9c8f593add3e2c
Jpie <s20015@gsm.hs.kr>
2022-09-12 10:16:16 +0900

    할일 추가 및 리드미 수정

--- kidnap/Assets/C# Scripts/UI/UI Manager.cs
+++ kidnap/Assets/C# Scripts/UI/UI Manager.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //설정
+    public Button setting;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //휴대폰
+    public Button phone;
+
+    //할일
+    public Button doList;
+
+    
 }
