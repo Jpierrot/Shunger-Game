380e7c5b5381f4150a3fd8a14feb3d59a685b513
Jpie <s20015@gsm.hs.kr>
2022-09-28 23:52:31 +0900

    SupportSystem Update
    코드 구조화 추가 작업
    함수를 분할해서 코드 가독성 및
    재사용성을 높임

--- kidnap/Assets/Scripts/Systems/SupportSystem.cs
+++ kidnap/Assets/Scripts/Systems/SupportSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace Kidnap
 {
@@ -14,58 +15,106 @@ namespace Kidnap
             A, B, C
         }
 
+        [HideInInspector]
         public int[] Favorability = new int[3] { 0, 0, 0 };
 
-        public int[] SupportPercent = new int[3] { 0, 0, 0 };
 
-        
+        int[] _supportPercent = new int[3] { 0, 0, 0 };
 
-        private Chars _favor = (Chars)Random.Range(0, System.Enum.GetValues(typeof(Chars)).Length);
+        [SerializeField]
+        private Chars _favor = (Chars)Random.Range(0, 2);
 
         private int _minPercent = 15;
 
         private int _allFavor = 0;
 
-        public void SupportCheck(Chars type, int favor) => 
-            Favorability[(int)type] = favor;
+        public string _countryName { get; set; }
 
-        public void CheckFavor()
+        /// <summary>
+        /// 해당 캐릭터에 대한 호감도를 증감하는 메소드
+        /// </summary>
+        /// <param name="type">캐릭터를 지정</param>
+        /// <param name="value">증감하는 호감도의 값</param>
+        public void SupportCheck(Chars type, int value)
+           => Favorability[(int)type] += value;
+
+        public void Init()
         {
-            foreach (int a in Favorability)
-            {
-                _allFavor += a;
-            }
+            SetSupport(_favor);
         }
 
-        // 호감도 : 지지율 => 특정 호감도 / 전체 호감도 * 전체 지지율
-        public void SetCountry()
+        
+
+        // 호감도 : 지지율 => 특정 후 호감도 / 전체 호감도 * 전체 지지율
+        private void SetSupport(Chars type)
         {
+            _allFavor = ArrayCalc(Favorability);
+
             for(int i = 0; i < 3; i++)
             {
                 var favor = Favorability[i];
 
-                if (favor == 0)
+                if (favor == 0 && i == (int)type)
                     _minPercent = 0;
 
-                SupportPercent[i] = (100 - _minPercent) * (favor / _allFavor);
+                _supportPercent[i] = FavorCalc(_minPercent, favor);
             }
+
+            _supportPercent[(int)_favor] += _minPercent;
             
         }
 
+        private int FavorCalc(int min, int favor)
+        {
+            float fav = (favor / _allFavor);
+            fav = (float)(100 - min) * fav;
 
+            return (int)fav;
+        }
+
+        private int ArrayCalc(int[] array)
+        {
+            int all = 0;
+
+            foreach (int value in array) all += value;
+
+            return all;
+        }
 
     }
 
 
     public class SupportSystem : Singleton<SupportSystem>
     {
+
+        public string[] CountryNames;
+
+        [HideInInspector]
         public List<Country> Countries;
 
+        void SetCountries()
+        {
+            //지역별 리스트 생성하기
+            for(int i = 0; i < CountryNames.Length; i++)
+            {
+                Countries.Add(new Country());
+            }
+
+            //Linq를 이용한 foreach구문 인덱스 구하기
+            foreach(var (value, i) in CountryNames.Select((value, i) => (value, i)))
+            { 
+                Countries[i]._countryName = value;
+                Debug.Log(Countries[i]._countryName + $" 인덱스 : {i}");
+            }
+
+            Debug.Log("인덱스 정렬 끝");
+        }
+        
 
 
         void Start()
         {
-            
+            SetCountries();
         }
 
         // Update is called once per frame
