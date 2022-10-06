379ddd575745cd7dd66519a27c5b7322a2bbcea9
Jpie <s20015@gsm.hs.kr>
2022-10-06 23:57:21 +0900

    코드 위치 수정

---
+++ kidnap/Assets/Scripts/Systems/NewsSystem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace kidnap
+{
+
+    public class NewsSystem : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+
+}
